bbdf211f94f27a41d9ee85c9c0a9203e42cf8a84
Carlos Felix-Beltran <felixbec@gmatc.matc.edu>
2019-02-07 21:03:12 -0600

    update element components

---
+++ components/.cache/sync-requires.js
@@ -0,0 +1,13 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/cfbeltran_it/Documents/WebProg2/components/beltran-carlos-components/components/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/cfbeltran_it/Documents/WebProg2/components/beltran-carlos-components/components/src/pages/404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/cfbeltran_it/Documents/WebProg2/components/beltran-carlos-components/components/src/pages/index.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/cfbeltran_it/Documents/WebProg2/components/beltran-carlos-components/components/src/pages/page-2.js")))
+}
+
